5797d24402e68e85bac9f1e92c2e34bdc1534be9
Noor-ul12 <165083231+Noor-ul12@users.noreply.github.com>
2024-04-24 00:33:43 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+// // 1) computer will generate a ramdom number
+
+// // 2) user input for gussing number
+
+// // 3) campare user number with computer ganerater number and show result
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    massage: "please guess a number between 1 to 10:",
+  },
+]);
+
+if(answer.userGuessedNumber === randomNumber) {
+    console.log("congratulation! you win you guess right number");
+} else {
+    console.log(" you lose you guess a worng number");
+}
